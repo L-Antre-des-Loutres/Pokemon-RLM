22179077e76220c855795cbac2e5a666e21dbf32
matheo-prd <perod.matheo@gmail.com>
2024-10-20 20:17:05 +0200

    Mapping de la map d'avant la ville grotte

---
+++ Data/Tiled/Tilesets/tileset-diamant-perle.tsx
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tileset-diamant-perle" tilewidth="32" tileheight="32" tilecount="10992" columns="8">
+ <image source="../Assets/tileset-diamant-perle.png" width="256" height="43968"/>
+ <wangsets>
+  <wangset name="Relief-Montagne" type="mixed" tile="-1">
+   <wangcolor name="Montagne-1" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="283" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="284" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="285" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="286" wangid="1,1,1,0,1,1,1,1"/>
+   <wangtile tileid="287" wangid="1,1,1,1,1,0,1,1"/>
+   <wangtile tileid="291" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="292" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="293" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="294" wangid="1,0,1,1,1,1,1,1"/>
+   <wangtile tileid="295" wangid="1,1,1,1,1,1,1,0"/>
+   <wangtile tileid="299" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="300" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="301" wangid="1,0,0,0,0,0,1,1"/>
+  </wangset>
+  <wangset name="Grotte" type="mixed" tile="-1">
+   <wangcolor name="Montagne" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="Montagne-2" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="1664" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="1665" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="1666" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="1668" wangid="0,0,2,2,2,0,0,0"/>
+   <wangtile tileid="1669" wangid="0,0,2,2,2,2,2,0"/>
+   <wangtile tileid="1670" wangid="0,0,0,0,2,2,2,0"/>
+   <wangtile tileid="1672" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="1673" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="1674" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="1676" wangid="2,2,2,2,2,0,0,0"/>
+   <wangtile tileid="1677" wangid="2,2,2,2,2,2,2,2"/>
+   <wangtile tileid="1678" wangid="2,0,0,0,2,2,2,2"/>
+   <wangtile tileid="1680" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="1681" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="1682" wangid="1,0,0,0,0,0,1,1"/>
+   <wangtile tileid="1684" wangid="2,2,2,0,0,0,0,0"/>
+   <wangtile tileid="1685" wangid="2,2,2,0,0,0,2,2"/>
+   <wangtile tileid="1686" wangid="2,0,0,0,0,0,2,2"/>
+   <wangtile tileid="1688" wangid="1,1,1,0,1,1,1,1"/>
+   <wangtile tileid="1689" wangid="1,1,1,1,1,0,1,1"/>
+   <wangtile tileid="1690" wangid="2,2,2,0,2,2,2,2"/>
+   <wangtile tileid="1691" wangid="2,2,2,2,2,0,2,2"/>
+   <wangtile tileid="1696" wangid="1,0,1,1,1,1,1,1"/>
+   <wangtile tileid="1697" wangid="1,1,1,1,1,1,1,0"/>
+   <wangtile tileid="1698" wangid="2,0,2,2,2,2,2,2"/>
+   <wangtile tileid="1699" wangid="2,2,2,2,2,2,2,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
