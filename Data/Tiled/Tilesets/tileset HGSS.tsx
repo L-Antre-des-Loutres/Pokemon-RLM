a30918eca02a930dba95f8b137ccb03e2a78dc36
matheo-prd <perod.matheo@gmail.com>
2024-11-14 00:33:41 +0100

    Fix des pokémons restants

---
+++ Data/Tiled/Tilesets/tileset HGSS.tsx
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tileset HGSS" tilewidth="32" tileheight="32" tilecount="7488" columns="8">
+ <image source="../../../../../../Downloads/1tls-f3-1db900e.png" width="256" height="29952"/>
+ <wangsets>
+  <wangset name="Montagne" type="mixed" tile="-1">
+   <wangcolor name="Montagne 1" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="Montagne Herbe" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="5872" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="5873" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="5874" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="5875" wangid="0,0,2,2,2,0,0,0"/>
+   <wangtile tileid="5876" wangid="0,0,2,2,2,2,2,0"/>
+   <wangtile tileid="5877" wangid="0,0,0,0,2,2,2,0"/>
+   <wangtile tileid="5878" wangid="1,1,1,0,1,1,1,1"/>
+   <wangtile tileid="5879" wangid="1,1,1,1,1,0,1,1"/>
+   <wangtile tileid="5880" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="5881" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="5882" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="5883" wangid="2,2,2,2,2,0,0,0"/>
+   <wangtile tileid="5884" wangid="2,2,2,2,2,2,2,2"/>
+   <wangtile tileid="5885" wangid="2,0,0,0,2,2,2,2"/>
+   <wangtile tileid="5886" wangid="1,0,1,1,1,1,1,1"/>
+   <wangtile tileid="5887" wangid="1,1,1,1,1,1,1,0"/>
+   <wangtile tileid="5888" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="5889" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="5890" wangid="1,0,0,0,0,0,1,1"/>
+   <wangtile tileid="5891" wangid="2,2,2,0,0,0,0,0"/>
+   <wangtile tileid="5892" wangid="2,2,2,0,0,0,2,0"/>
+   <wangtile tileid="5893" wangid="2,0,0,0,0,0,2,2"/>
+   <wangtile tileid="5894" wangid="2,2,2,0,2,2,2,2"/>
+   <wangtile tileid="5895" wangid="2,2,2,2,2,0,2,2"/>
+   <wangtile tileid="5902" wangid="2,0,2,2,2,2,2,2"/>
+   <wangtile tileid="5903" wangid="2,2,2,2,2,2,2,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
