fcc92440561a6c720cc437891affb35bb385cc3d
corentin-cott <corentin.cott@gmail.com>
2025-02-17 10:41:24 +0100

    Ajout des ressourses de Pokémon Platine (Par SirMalo)

---
+++ Data/Tiled/Tilesets/PLAT borders.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Délimitations" tilewidth="32" tileheight="32" tilecount="15" columns="5">
+ <image source="Limites.png" trans="40cb51" width="160" height="96"/>
+</tileset>
