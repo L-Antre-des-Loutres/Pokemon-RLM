d28fb4e78a7dca14574ddc1d1d543dd009195943
Corentin COTTEREAU <109530954+Corentin-cott@users.noreply.github.com>
2025-05-06 22:40:56 +0200

    Ajout de tests mapping

---
+++ Data/Tiled/Tilesets/RLM-VilleChateau.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="RLM-VilleChateau" tilewidth="32" tileheight="32" tilecount="420" columns="30">
+ <image source="../Assets/RLM/RLM-VilleChateau.png" width="960" height="448"/>
+</tileset>
